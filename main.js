2e794f7b94883de728bf3f840d7563057017ea76
jesus <test@fakemail.com>
2021-05-28 10:31:36 -0400

    menu-responsive-prueba

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+
+// addEventListener('DOMContentLoaded', () => {
+
+//     const btn_menu = document.querySelector('.btn_menu')
+
+//     if (btn_menu){
+//         btn_menu.addEventListener('click', () => {
+//         const menu_items = document.querySelector('.menu_items')
+//         menu_items.classList.toggle('show')
+//         })
+//     }
+
+
+// })
+
+
+function showmenu() {
+    document.getElementById("hola").classList.toggle("show");
+  }
+
+
+//   agregue en el html un id con el nombre de hola y agregue un onclick con el nomnbre de showmenu para llamar la funcion , el toggle show es el del css
